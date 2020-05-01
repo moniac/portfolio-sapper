6311e7d25f026c44d2f33831dcb72de5d0a63668
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-01 12:42:03 +0200

    add minify

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,27 @@
-const tailwindcss = require("tailwindcss");
+const tailwindcss = require('tailwindcss');
 
 // only needed if you want to purge
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.svelte", "./src/**/*.html"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: ['./src/**/*.svelte', './src/**/*.html'],
+	defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
+const cssnano = require('cssnano')({
+	preset: [
+		'default',
+		{
+			discardComments: {
+				removeAll: true,
+			},
+		},
+	],
 });
 
 module.exports = {
-  plugins: [
-    tailwindcss("./tailwind.js"),
+	plugins: [
+		tailwindcss('./tailwind.js'),
 
-    // only needed if you want to purge
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+		// only needed if you want to purge
+		...(process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []),
+	],
 };
